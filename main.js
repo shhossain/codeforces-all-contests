f2dab05c1c793f2129e1549006d6902d32af150a
Sifat <hossain0338@gmail.com>
2022-12-03 22:33:46 +0600

    advenced search

--- main.js
+++ main.js
@@ -13,6 +13,17 @@ const monthMap = {
   Dec: "12",
 };
 
+class DefaultDict {
+  constructor(defaultVal) {
+    return new Proxy(
+      {},
+      {
+        get: (target, name) => (name in target ? target[name] : defaultVal),
+      }
+    );
+  }
+}
+
 class Contest {
   constructor(name, url, time, length, participants) {
     this.name = name;
@@ -58,47 +69,57 @@ function matchName(name, query) {
 }
 
 function matchTime(timeFormat, queryFormat) {
+  //   console.log("queryFormat", queryFormat);
   // Oct/11/2010 17:00 normal format
-  // query canbe any format
-  // if / or - or . or space is used, it will be converted to /
+  // queryFormat mm/dd/yyyy
 
-  // query can be MM/DD/YY or DD/MM/YY or YY/MM/DD
-  let years = [];
-  let months = [];
-  let days = [];
+  // check if " " is present then match time and name
 
-  // query
-  let query = queryFormat.split(/[-./ ]/);
-  for (let i = 0; i < query.length; i++) {
-    years.push(query[i]);
-    months.push(query[i]);
-    days.push(query[i]);
+  let time = timeFormat.split(" ")[0].split("/");
+  let query = queryFormat.split("/");
+
+  // check if query[0] is alphabets
+  if (isNaN(query[0])) {
+    let v = query[0].slice(0, 3);
+    v = v.charAt(0).toUpperCase() + v.slice(1);
+    query[0] = monthMap[v];
   }
 
-  // search
-  let ans = [0, 0, 0];
-  let search = timeFormat.split(/[-./ ]/);
-  for (let i = 0; i < search.length; i++) {
-    for (let j = 0; j < years.length; j++) {
-      if (search[i] === years[j]) {
-        ans[0] = 1;
-      }
-    }
-    for (let j = 0; j < months.length; j++) {
-      if (search[i] === months[j]) {
-        ans[1] = 1;
-      }
-    }
+  let tmonth = parseInt(monthMap[time[0]]);
+  let tday = parseInt(time[1]);
+  let tyear = parseInt(time[2]) % 2000;
 
-    for (let j = 0; j < days.length; j++) {
-      if (search[i] === days[j]) {
-        ans[2] = 1;
-      }
-    }
+  let qmonth = parseInt(query[0]);
+  let qday = parseInt(query[1]);
+  let qyear = parseInt(query[2]) % 2000;
+
+  if (qmonth == tmonth && !qday && !qyear) {
+    return true;
+  } else if (qmonth == tmonth && qday == tday && !qyear) {
+    return true;
+  } else if (qmonth == tmonth && !qday && qyear == tyear) {
+    return true;
+  } else if (qday == tday && !qmonth && !qyear) {
+    return true;
+  } else if (qday == tday && !qmonth && qyear == tyear) {
+    return true;
+  } else if (qyear == tyear && !qmonth && !qday) {
+    return true;
   }
 
-  // if sum is atleast 2, then it is a match
-  if (ans[0] + ans[1] + ans[2] >= 2) {
+  let ans = 0;
+
+  if (Math.abs(qmonth - tmonth) <= 1) {
+    ans += 1;
+  }
+  if (Math.abs(qday - tday) <= 10) {
+    ans += 1;
+  }
+  if (qyear == tyear) {
+    ans += 1;
+  }
+
+  if (ans == 3) {
     return true;
   }
 
@@ -169,6 +190,10 @@ const contestsTable = document.getElementById("contests");
 const search = document.getElementById("search");
 let currentContests = contests;
 let loaded = false;
+let searching = false;
+let currentSearchQuery = "";
+let previousSearchQuery = new DefaultDict(0);
+let MAX_SEARCH_QUERY = 20;
 
 function showContests(contests) {
   // show contests in table
@@ -196,19 +221,121 @@ showContests(contests);
 
 // advanced search
 function advancedSearch(contests, searchString) {
+  if (previousSearchQuery[searchString] != 0) {
+    return previousSearchQuery[searchString];
+  }
+
+  if (searching) {
+    return;
+  }
+
   // if / or -  or space is used, then match time else match name
+  searching = true;
   let filteredContests = [];
-  if (searchString.includes("/") || searchString.includes("-")) {
+  let isNot = false;
+  let isNotq = "";
+  if (searchString.includes("not ")) {
+    isNot = true;
+    isNotq = searchString.split("not ")[1];
+    let replacev = "not " + isNotq;
+    searchString = searchString.replace(replacev, "");
+  }
+
+  if (searchString.includes(" or ")) {
+    // or
+    let queries = searchString.split(" or ");
+    for (let i = 0; i < queries.length; i++) {
+      let query = queries[i];
+      // time and name
+      if (query.includes(" ") && query.includes("/")) {
+        let splits = query.split(" ");
+        let time = splits[splits.length - 1];
+        let name = splits.slice(0, splits.length - 1).join(" ");
+        for (let j = 0; j < contests.length; j++) {
+          let contest = contests[j];
+          if (matchTime(contest.time, time) && matchName(contest.name, name)) {
+            filteredContests.push(contest);
+          }
+        }
+      } else if (query.includes("/")) {
+        // time
+        for (let j = 0; j < contests.length; j++) {
+          let contest = contests[j];
+          if (matchTime(contest.time, query)) {
+            filteredContests.push(contest);
+          }
+        }
+      } else {
+        // name
+        for (let j = 0; j < contests.length; j++) {
+          let contest = contests[j];
+          if (matchName(contest.name, query)) {
+            filteredContests.push(contest);
+          }
+        }
+      }
+    }
+  } else if (searchString.includes("/") && searchString.includes(" ")) {
+    // match time and name
+    let splits = searchString.split(" ");
+    // time is last
+    let time = splits[splits.length - 1];
+    let name = splits.slice(0, splits.length - 1).join(" ");
+    for (let i = 0; i < contests.length; i++) {
+      let contest = contests[i];
+      if (matchName(contest.name, name) && matchTime(contest.time, time)) {
+        filteredContests.push(contest);
+      }
+    }
+  } else if (searchString.includes("/") || searchString.includes("-")) {
     filteredContests = contests.filter((contest) => {
       return matchTime(contest.time, searchString);
     });
-  } else {
+  } else if (searchString.length > 2) {
     filteredContests = contests.filter((contest) => {
       return matchName(contest.name, searchString);
     });
   }
+  if (isNot) {
+    let filteredContests2 = [];
+    if (isNotq.includes("/") && isNotq.includes(" ")) {
+      // match time and name
+      let splits = isNotq.split(" ");
+      // time is last
+      let time = splits[splits.length - 1];
+      let name = splits.slice(0, splits.length - 1).join(" ");
+      for (let i = 0; i < filteredContests.length; i++) {
+        let contest = filteredContests[i];
+        if (!matchName(contest.name, name) && !matchTime(contest.time, time)) {
+          filteredContests2.push(contest);
+        }
+      }
+    } else if (isNotq.includes("/") || isNotq.includes("-")) {
+      filteredContests2 = filteredContests.filter((contest) => {
+        return !matchTime(contest.time, isNotq);
+      });
+    } else {
+      filteredContests2 = filteredContests.filter((contest) => {
+        return !matchName(contest.name, isNotq);
+      });
+    }
+    filteredContests = filteredContests2;
+  }
+
+  previousSearchQuery[searchString] = filteredContests;
+  // if previous search query is more than 10, then remove the oldest one
+  if (Object.keys(previousSearchQuery).length > MAX_SEARCH_QUERY) {
+    let keys = Object.keys(previousSearchQuery);
+    delete previousSearchQuery[keys[0]];
+  }
+
+  searching = false;
   return filteredContests;
 }
+
+// function search(contests, query) {
+
+// }
 
 // sort by name, time, length, participants
 function sortContests(sort, asc = false) {
@@ -278,12 +405,15 @@ function removeOtherActiveButtons(button) {
 }
 
 // event listeners
-
 // search bar event listener
 search.addEventListener("keyup", (e) => {
-  const searchString = e.target.value.toLowerCase();
+  const searchString = e.target.value.toLowerCase().trim();
   // min 3 characters
-  if (searchString.length > 2) {
+  if (searchString.length > 0) {
+    if (searchString == currentSearchQuery) {
+      return;
+    }
+    currentSearchQuery = searchString;
     const filteredContests = advancedSearch(contests, searchString);
     if (filteredContests) {
       showContests(filteredContests);
@@ -317,3 +447,65 @@ sortButtons.forEach((button) => {
     }
   });
 });
+
+// search guide
+const searchGuideButton = document.querySelector("#search-guide-button");
+searchGuideButton.addEventListener("click", () => {
+  searchGuideButton.classList.toggle("d-none");
+  showSearchGuide();
+});
+
+function showSearchGuide() {
+  let guide = `
+    <h4>Search for contests by name, date</h4>
+    <h5>Examples:</h5>
+    div4 MM/DD/YYYY
+    div4 10/10/2020
+    div4 10//2020
+    div4 //2020
+    div4 //22
+    div3 feb/
+    div4 or div3
+    div4 or div3 not upcoming
+    div4 or div3 not //2020
+    `;
+  // add text-muted to every line
+  guide = guide
+    .split("\n")
+    .map((line) => {
+      return `<p class="text-muted">${line}</p>`;
+    })
+    .join("");
+
+  // show search guide in black not with white text, "or","not" should be in red
+
+  // show modal
+  // create modal on the fly
+  const modal = `
+    <div class="modal fade" id="searchGuideModal" tabindex="-1" aria-labelledby="searchGuideModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="searchGuideModalLabel">Search Guide</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              ${guide}
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+                </div>
+                </div>
+                </div>
+                `;
+
+  // add modal to body
+  document.body.innerHTML += modal;
+  // show modal
+
+  const searchGuideModal = new bootstrap.Modal(
+    document.getElementById("searchGuideModal")
+  );
+  searchGuideModal.show();
+}
